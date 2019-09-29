31740b86cf7999c12bb677d9617637581d2a924b
xindong <1477015648@qq.com>
2019-09-29 16:48:18 +0800

    测试添加git

---
+++ web-api/web/src/main/java/com/lunz/cpfw/web/services/tb_product_vehicletypeService.java
@@ -0,0 +1,13 @@
+package com.lunz.cpfw.web.services;
+
+import com.lunz.cpfw.core.service.ServiceBase;
+import com.lunz.cpfw.web.entities.tb_product_vehicletype;
+import com.lunz.cpfw.web.interfaces.Itb_product_vehicletypeService;
+import com.lunz.cpfw.web.mappers.tb_product_vehicletypeMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class tb_product_vehicletypeService extends ServiceBase<tb_product_vehicletypeMapper, tb_product_vehicletype>
+implements Itb_product_vehicletypeService {
+
+}
